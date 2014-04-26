ef0e23e6b0ea2f75a40a8a1a676686c486b616ee
Peggy Griffin <seishingithub@gmail.com>
2014-04-26 14:32:36 -0600

    Destroying kitties

--- application.rb
+++ application.rb
@@ -20,8 +20,24 @@ class Application < Sinatra::Application
   end
 
   post '/cats' do
-    p params
     DB[:cats].insert(:name => params[:name], :color => params[:color], :kittens => params[:kittens])
+    redirect '/'
+  end
+
+  get '/cats/:id' do
+    cat_id = params[:id]
+    erb :show, locals: { single_cat: DB[:cats][id: cat_id]}
+  end
+
+  put '/cats/:id' do
+    cat_id = params[:id]
+    DB[:cats].where(id: cat_id).update(name: params[:name], color: params[:color], kittens: params[:kittens])
+    redirect "/cats/#{cat_id}" # is the same as: redirect "/cats/#{params[:id]}"
+  end
+
+  delete '/cats/:id' do
+    cat_id = params[:id] # or... delete this line and...
+    DB[:cats].where(id: cat_id).delete # or... DB[:cats].where(id: params[:id]).delete
     redirect '/'
   end
 end
--- spec/features/application_spec.rb
+++ spec/features/application_spec.rb
@@ -13,4 +13,28 @@ feature 'Homepage' do
     click_on 'Create Cat'
     expect(page).to have_content 'Siamese'
   end
+
+  scenario "can update a cat" do
+    visit '/cats/new'
+    fill_in 'name', with: 'Siamese'
+    fill_in 'color', with: 'White'
+    fill_in 'kittens', with: 4
+    click_on 'Create Cat'
+    click_on 'Siamese'
+    fill_in 'name', with: 'Lola'
+    click_on 'Update Kitty'
+    expect(page).to have_content('Lola')
+    expect(page).to_not have_content('Siamese')
+  end
+
+  scenario "can destroy a kitty" do
+    visit '/cats/new'
+    fill_in 'name', with: 'Siamese'
+    fill_in 'color', with: 'White'
+    fill_in 'kittens', with: 4
+    click_on 'Create Cat'
+    click_on 'Siamese'
+    click_on 'Destroy Kitty'
+    expect(page).to_not have_content('Siamese')
+  end
 end
